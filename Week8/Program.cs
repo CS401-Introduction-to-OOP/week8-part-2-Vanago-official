571bb397ac93dd38db12cde3160d22c3fb0bb51a
Vanago-official <vananovoselov824@gmail.com>
2026-03-28 17:38:39 +0200

    Stage 3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,25 @@
-﻿
+﻿namespace Week8;
+
 class Program
 {
     public static void Main()
     {
-        
+        var container = new DataContainer<DataItem>();
+        for (int i = 0; i < 10000; i++)
+        {
+            var item = new DataItem
+            {
+                Id = $"item-{i}",
+                Payload = new byte[1024]
+            };
+            // TODO: add item to container
+        container.AddItem(item);
+        }
+
+        Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+        // TODO: call GC.Collect()
+        GC.Collect();
+        Console.WriteLine("GC.Collect() was called.");
+        Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes");
     }
-    
 }
