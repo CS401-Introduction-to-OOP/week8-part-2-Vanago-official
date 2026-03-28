259bf0c0b8175c5a435be3cfbba426b79227d854
Vanago-official <vananovoselov824@gmail.com>
2026-03-28 17:19:40 +0200

    Stage 1

---
+++ Week8/Point.cs
@@ -0,0 +1,13 @@
+namespace Week8;
+
+public struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public void Print()
+    {
+// TODO: print coordinates
+        Console.WriteLine($"X: {X}\n Y: {Y}");
+    }
+}
---
+++ Week8/PointRef.cs
@@ -0,0 +1,13 @@
+namespace Week8;
+
+public class PointRef
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public void Print()
+    {
+// TODO: print coordinates
+        Console.WriteLine($"X: {X}\n Y: {Y}");
+    }
+}
